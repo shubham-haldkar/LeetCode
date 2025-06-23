6d3dbc9b701ea09bec477c4c77b5d6b208c60c7a
Shubham-Haldkar <shubham.haldkar@gmail.com>
2025-06-23 20:23:29 +0530

    day 5

---
+++ reverse-words-in-a-string.java
@@ -0,0 +1,12 @@
+//  https://leetcode.com/problems/reverse-words-in-a-string/submissions/
+class Solution {
+    public String reverseWords(String s) {
+        String[] rev = s.split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = rev.length - 1; i >= 0; i--) {
+            result.append(rev[i].trim()  );
+            if (i != 0) result.append( " ");
+        }
+        return result.toString().trim();
+    }
+}
