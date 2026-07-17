6b9a8da6028501d3649504c6c0c246e509f4f1ba
Shubham-Haldkar <shubham.haldkar@gmail.com>
2026-07-17 18:41:56 +0530

    coinChange

---
+++ Medium/coinChangeMain.java
@@ -0,0 +1,23 @@
+package Medium;
+
+public class coinChangeMain {
+    
+     public int coinChange(int[] coins, int amount) {
+        
+        int[] dp = new int[amount+1] ;
+
+        for(int i = 0 ; i<=amount ; i++ ){
+            dp[i] = amount+1 ;
+        }
+        
+        dp[0] = 0 ;
+
+        for( int coin : coins){
+            for(int i=coin; i<= amount ; i++ ){
+                dp[i] = Math.min(dp[i], dp[i-coin] +1) ;
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
