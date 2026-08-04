c36c2d93ccbe6d4c8e469ed52df79c3bb0f264c0
Shubham-Haldkar <shubham.haldkar@gmail.com>
2026-08-04 22:24:31 +0530

    FindMissingElements

---
+++ Easy/FindMissingElements.java
@@ -0,0 +1,34 @@
+package Easy;
+
+public class FindMissingElements{
+
+    public static void main(String[] args) {
+        
+    }
+
+     public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
+
+        for(int i : nums){
+            max = Math.max(i, max);
+            min = Math.min(i, min);
+        }
+
+        int[] arr = new int[max + 1];
+        for(int i : nums){
+            arr[i]++;
+        }
+
+        for(int i=min;i<max;i++){
+            if(arr[i] == 0)
+                ans.add(i);
+        }
+        return ans;
+
+
+    }
+
+}
