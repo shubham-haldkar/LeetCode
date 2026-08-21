8c1681a13515994b3cf3d4110be830edcf85f478
Shubham-Haldkar <shubham.haldkar@gmail.com>
2026-08-21 11:01:21 +0530

    3069. Distribute Elements Into Two Arrays I

---
+++ Easy/DistributeElementsIntoTwoArraysI.java
@@ -0,0 +1,44 @@
+package Easy;
+
+// 3069. Distribute Elements Into Two Arrays I
+
+class DistributeElementsIntoTwoArraysI {
+    static {
+        for(int i = 0; i <= 500; i++) {
+            resultArray(new int[2]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 3, 3};
+        int[] result = resultArray(nums);
+        for(int i : result) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] resultArray(int[] nums) {
+        int n = nums.length;
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n-1];
+        arr1[0] = nums[0];
+        arr2[0] = nums[1];
+        // Like a stack pointer
+        int arr1Pointer = 0;
+        int arr2Pointer = 0;
+        for(int i = 2; i < n; i++) {
+            if(arr1[arr1Pointer] > arr2[arr2Pointer]) {
+                arr1[++arr1Pointer] = nums[i];
+            } else {
+                arr2[++arr2Pointer] = nums[i];
+            }
+        }
+        for(int i = 0; i <= arr2Pointer; i++) {
+            arr1[++arr1Pointer] = arr2[i];
+        }
+        return arr1;
+    }
+
+    
+}
+
