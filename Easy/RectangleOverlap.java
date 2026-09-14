ab119dff7611bb9525f36c9c52639267e3b7c6cb
Shubham Haldkar <67072200+shubham-haldkar@users.noreply.github.com>
2026-09-14 18:39:33 +0530

    836. Rectangle Overlap

---
+++ Easy/RectangleOverlap.java
@@ -0,0 +1,15 @@
+// 836. Rectangle Overlap
+
+public class RectangleOverlap {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+        // 1D X-axis projection check
+        boolean xOverlap = Math.max(rec1[0], rec2[0]) < Math.min(rec1[2], rec2[2]);
+        
+        // 1D Y-axis projection check
+        boolean yOverlap = Math.max(rec1[1], rec2[1]) < Math.min(rec1[3], rec2[3]);
+
+        return xOverlap && yOverlap;
+
+    }
+}
